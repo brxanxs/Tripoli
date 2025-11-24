1bd6a42f59f6c4b21bf9219905ccb1f6d351e9d7
Tong02045440 <tongsean2004@gmail.com>
2025-11-23 23:04:09 -0500

    modify reporter lambda to create csv

--- lambda/reporter.py
+++ lambda/reporter.py
@@ -24,14 +24,34 @@ def lambda_handler(event, context):
                 if last_mod > time_prev:
                     filename_list.append({'filename' : obj['Key'], 'uploaded' : last_mod })
     
-    buffer = io.StringIO()
-    writer = csv.writer(buffer)
+    stream = io.StringIO()
+    writer = csv.writer(stream)
     header = ['Filename', 'Date_Uploaded']
     writer.writerow(header)
 
     for row in filename_list:
         writer.writerow([row['filename', 'uploaded']])
 
-    content = buffer.getvalue()
+    content = stream.getvalue()
+
+    time_now = datetime.now(timezone.utc)
+    bucket_key = f'report-{time_now}.csv'
+    
+    s3.Bucket(OUT_BUCKET).put_object(
+        Key = bucket_key,
+        Body = content,
+        ContentType = 'text/csv'
+    )
+
+    url = s3.generate_presigned_url(
+        ClientMethod = 'get_object',
+        Params = {'Bucket': OUT_BUCKET, 'Key': bucket_key},
+        ExpiresIn = 86400
+    )
+
+
+    subject = 'File Report'
+    body = f'{url}'
 
     
+
